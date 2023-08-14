677691b244e7d500d86053159f5aabc50b2be142
cmh1506 <cmh1506@yahoo.de>
2023-08-14 23:43:58 +0200

    nodemon etc

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const express = require('express');
+const express = require('express')
+const morgan = require('morgan')
+const debug = require('debug')('app')
+const app = express()
+const path = require('path')
 
-const app = express();
+app.use(morgan('tiny'))
+app.use(express.static(path.join(__dirname, '/public/')))
 
 app.get('/', (req, res) => {
   res.send('Liebling, es ist aus.')
 })
 
 app.listen(3000, () => {
-  console.log('Listening on 3000')
+  debug('Listening on 3000')
 })
