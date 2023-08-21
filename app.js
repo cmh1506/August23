d0eec2a9edecdfa651beb2c6324062ba3ec21471
cmh1506 <cmh1506@yahoo.de>
2023-08-21 17:36:33 +0200

    azure connection string

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const debug = require('debug')('app')
 const app = express()
+
+
+const db = mongoose.connect('mongodb://spocco2-server:jXgQsiud9ycxa0IxE87REIibpP84s56hCo1adPXxW7zbjabt2q3uHETeCsdNO2KkltlXiMHWWjAGACDbkCpsPg==@spocco2-server.mongo.cosmos.azure.com:10255/spocco2-database?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@spocco2-server@')
 //const db = mongoose.connect('mongodb://mongodb/spoc')
-if (process.env.ENV === 'Test') {
+/* if (process.env.ENV === 'Test') {
   console.log("This is a test.")
   const db = mongoose.connect('mongodb://127.0.0.1/spoc_test')
 } else if ( process.argv[2] === 'int') {
@@ -14,7 +17,7 @@ if (process.env.ENV === 'Test') {
 }  else {  
   console.log("Bin in Docker")
   const db = mongoose.connect('mongodb://mongodb/spoc')
-}
+} */
 
 const path = require('path')
 const port = process.env.PORT || 3000
